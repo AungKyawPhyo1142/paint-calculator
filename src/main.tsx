25279c250ceac45073a887d602665a5d1d647c50
Aung Kyaw Phyo (Sebastian Kein) <94116245+AungKyawPhyo1142@users.noreply.github.com>
2024-09-25 05:08:48 +0700

    init: project structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-// Function to wrap a functional React component as a Web Component
-function createReactWebComponent(
-  Component: React.FC,
-  elementName: string
-): void {
-  class ReactElement extends HTMLElement {
-    mountPoint: HTMLDivElement;
-    root: ReactDOM.Root | null = null;
-    constructor() {
-      super();
-      this.mountPoint = document.createElement("div");
-    }
-
-    connectedCallback(): void {
-      this.attachShadow({ mode: "open" });
-      this.shadowRoot?.appendChild(this.mountPoint);
-      // Render the functional React component inside the Web Component
-      this.root = ReactDOM.createRoot(this.mountPoint);
-      this.root.render(<Component />);
-    }
-
-    disconnectedCallback(): void {
-      if (this.root) {
-        this.root.unmount();
-      }
-    }
-  }
-
-  window.customElements.define(elementName, ReactElement);
-}
-
-// Register the functional component as a Web Component
-createReactWebComponent(App, "paint-calculator");
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/paint-calculator/',
-  build: {
-    lib: {
-      entry: "./src/main.tsx", // The entry point of your React app
-      name: "PaintCalculator",
-      fileName: (format) => `paint-calculator.${format}.js`,
-    },
-    rollupOptions: {
-      // Make sure to externalize dependencies that shouldn't be bundled
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
-  },
-});
+})
