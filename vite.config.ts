a2fad16d01b2218f1505d666ee3761c4ba76f11f
Aung Kyaw Phyo (Sebastian Kein) <94116245+AungKyawPhyo1142@users.noreply.github.com>
2024-09-25 04:58:07 +0700

    init: project structure

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'paint-calculator',
   build: {
     lib: {
       entry: "./src/main.tsx", // The entry point of your React app
