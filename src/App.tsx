77dab4ee1066bb44e4207e1f7cba6836561e73a8
Aung Kyaw Phyo (Sebastian Kein) <94116245+AungKyawPhyo1142@users.noreply.github.com>
2024-09-26 08:25:52 +0700

    feat: add logic for ceiling.Paint

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
   const [area, setArea] = useState<string>("0.00");
   const [amountOfPrimer, setAmountOfPrimer] = useState<number>(0);
   const [amountOfPaint, setAmountOfPaint] = useState<number>(0);
+  const [amountOfCeilingPaint, setAmountOfCeilingPaint] = useState<number>(0);
 
   const handleRoomTypeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setRoomType(e.target.value);
@@ -31,7 +32,7 @@ function App() {
   const AREA_ONE_GALLON_OF_PRIMER_CAN_COVER = 40; // m^2
   const NUMBER_OF_TIMES_FOR_PAINT = 2;
   const AREA_ONE_GALLON_OF_PAINT_CAN_COVER = 40; // m^2
-  const BUFFER = 0.15; //15% buffer
+  const BUFFER = 1.15; //15% buffer
 
   // calculate total area for whole room
   useEffect(() => {
@@ -47,7 +48,7 @@ function App() {
         DOORS_AREA * doors[0] -
         WINDOWS_AREA * windows[0]
       ).toFixed(2);
-    } else {
+    } else if (roomType === "singleWall") {
       // single wall
       Rarea = (parseFloat(length) * parseFloat(height)).toFixed(2);
 
@@ -56,6 +57,9 @@ function App() {
         DOORS_AREA * doors[0] -
         WINDOWS_AREA * windows[0]
       ).toFixed(2);
+    } else {
+      // whole room with ceiling
+      Rarea = (parseFloat(width) * parseFloat(length)).toFixed(2);
     }
 
     // primer
@@ -63,19 +67,25 @@ function App() {
       (parseFloat(Rarea) / AREA_ONE_GALLON_OF_PRIMER_CAN_COVER) *
       NUMBER_OF_TIMES_FOR_PRIMER
     ).toFixed(2);
-    primerAmt = Math.ceil(parseFloat(primerAmt) * (1 + BUFFER)).toFixed(2);
+    primerAmt = Math.ceil(parseFloat(primerAmt) * BUFFER).toFixed(2);
 
     // paint
     let paintAmt = (
       (parseFloat(Rarea) / AREA_ONE_GALLON_OF_PAINT_CAN_COVER) *
       NUMBER_OF_TIMES_FOR_PAINT
     ).toFixed(2);
+    paintAmt = Math.ceil(parseFloat(paintAmt) * BUFFER).toFixed(2);
 
-    paintAmt = Math.ceil(parseFloat(paintAmt) * (1 + BUFFER)).toFixed(2);
+    // ceiling paint
+    const ceilingPaint = (
+      (parseFloat(Rarea) * NUMBER_OF_TIMES_FOR_PAINT * BUFFER) /
+      AREA_ONE_GALLON_OF_PAINT_CAN_COVER
+    ).toFixed(2);
 
     setArea(Rarea);
     setAmountOfPrimer(Math.ceil(parseFloat(primerAmt)));
     setAmountOfPaint(Math.ceil(parseFloat(paintAmt)));
+    setAmountOfCeilingPaint(Math.ceil(parseFloat(ceilingPaint)));
   }, [
     length,
     height,
@@ -93,16 +103,16 @@ function App() {
       <div className=" min-h-screen w-full flex flex-col items-center justify-center font-noto-thai px-60 py-10">
         <div className=" px-10  w-full">
           <h1 className="text-[#013149] text-[55px] text-center athiti-regular">
-            How much paint do I need?
+            ต้องใช้สีเท่าไหร่ดี?
           </h1>
           <div className="mt-8">
-            <h3 className="text-xl text-[#000]">I'm painting a...</h3>
+            <h3 className="text-xl text-[#000]">กำลังทาสี...</h3>
             <div className="mt-3 flex gap-x-6 items-center">
               <div className="flex items-center gap-x-2">
                 <RadioBox
                   name="roomType"
                   id="singleWall"
-                  label="Single wall"
+                  label="ผนังเดียว"
                   checked={roomType === "singleWall"}
                   onChange={handleRoomTypeChange}
                   value="singleWall"
@@ -112,10 +122,20 @@ function App() {
                 <RadioBox
                   name="roomType"
                   id="wholeRoom"
-                  label="Whole Room"
+                  label="ทั้งห้อง"
                   checked={roomType === "wholeRoom"}
                   onChange={handleRoomTypeChange}
                   value="wholeRoom"
+                />
+              </div>
+              <div className="flex items-center gap-x-2">
+                <RadioBox
+                  name="roomType"
+                  id="wholeRoomWithCel"
+                  label="ทั้งห้องรวมถึงเพดาน"
+                  checked={roomType === "wholeRoomWithCel"}
+                  onChange={handleRoomTypeChange}
+                  value="wholeRoomWithCel"
                 />
               </div>
             </div>
@@ -133,28 +153,13 @@ function App() {
                 windows={windows}
                 setWindows={setWindows}
               />
-              {/* {roomType === "singleWall" ? (
-                <SingleWall
-                  selectedType={roomType}
-                  length={length}
-                  setLength={setLength}
-                  height={height}
-                  setHeight={setHeight}
-                  doors={doors}
-                  setDoors={setDoors}
-                  windows={windows}
-                  setWindows={setWindows}
-                />
-              ) : (
-                <WholeRoom />
-              )} */}
             </div>
             <div className="mt-10 bg-[#f5f9fa] text-[#013149] w-full min-h-[200px] flex flex-col justify-between gap-y-5 p-5">
               <div className="flex flex-col gap-y-2">
                 <div className="flex gap-x-2 items-center">
-                  <span className="text-base">Your wall area...</span>
+                  <span className="text-base">พื้นที่ผนัง...</span>
                   <a className="walls-tooltip">
-                    <InfoIcon className="w-4 h-4 mt-[1px]" />
+                    <InfoIcon className="w-4 h-4 " />
                   </a>
                   <Tooltip anchorSelect=".walls-tooltip" place="right">
                     <p>
@@ -170,32 +175,39 @@ function App() {
               <div className="flex flex-col gap-y-2">
                 <div className="flex gap-x-2 items-center">
                   <span className="text-base">
-                    For two coats, you'll need...
+                    สำหรับการทาสองชั้น คุณจะต้องใช้...
                   </span>
                   <a className="paint-tooltip">
-                    <InfoIcon className="w-4 h-4 mt-[1px]" />
+                    <InfoIcon className="w-4 h-4 " />
                   </a>
                   <Tooltip anchorSelect=".paint-tooltip" place="right">
                     <p>Enough for two coats, with +10% allowance included</p>
                   </Tooltip>
                 </div>
                 <h1 className="text-3xl font-[500]">
-                  {amountOfPrimer} gallons of Primer
+                  สีรองพื้น {amountOfPrimer} แกลลอน
                 </h1>
                 <h1 className="text-3xl font-[500]">
-                  {amountOfPaint} gallons of Paint
+                  สีทับหน้า {amountOfPaint} แกลลอน
                 </h1>
+                {roomType === "wholeRoomWithCel" && (
+                  <h1 className="text-3xl font-[500]">
+                    สีทาเพดาน {amountOfCeilingPaint} แกลลอน
+                  </h1>
+                )}
               </div>
             </div>
             <div className="mt-4 px-3">
               <h1 className="text-xl font-[500]">Heads up</h1>
               <p className="mt-2 text-sm">
-                This calculator is helpful, but just a guide. It’s accurate but
-                some of your specific measurements (like windows or doors) might
-                affect the results a bit. Happily, we offer super fast delivery
-                if you fall short - and returns if you over-egg it. Get in touch
-                if you’d like some more help from our friendly team: [email
-                here]
+                เครื่องคำนวณนี้ช่วยได้เยอะ แต่เป็นเพียงแนวทางคร่าวๆ
+                เท่านั้นนะครับ/ค่ะ ถึงแม้จะค่อนข้างแม่นยำ
+                แต่การวัดบางอย่างของคุณ (เช่น หน้าต่างหรือประตู)
+                อาจทำให้ผลลัพธ์เปลี่ยนไปเล็กน้อย
+                โชคดีที่เรามีบริการจัดส่งรวดเร็ว ถ้าสั่งไม่พอ –
+                หรือคืนได้ถ้าสั่งเยอะเกิน
+                ติดต่อทีมงานใจดีของเราได้เลยหากต้องการความช่วยเหลือเพิ่มเติม:
+                hello@voqepaint.com
               </p>
             </div>
           </div>
--- src/pages/RenderCalculator.tsx
+++ src/pages/RenderCalculator.tsx
@@ -34,55 +34,58 @@ const RenderCalculator: React.FC<Props> = ({
     <div className=" mt-8">
       <div className="flex flex-col gap-y-7">
         <div className="flex flex-col gap-y-3">
-          <h1 className="text-xl">Length</h1>
+          <h1 className="text-xl">ความยาว L</h1>
           <div className="flex gap-x-3 w-full">
             <NumericInput value={length} setValue={setLength} />
             <span className="text-gray-500 mt-0.5 text-lg font-[500]">m</span>
           </div>
         </div>
-        <div className="flex flex-col gap-y-3">
-          <h1 className="text-xl">Height</h1>
-          <div className="flex gap-x-3 w-full">
-            <NumericInput value={height} setValue={setHeight} />
-            <span className="text-gray-500 mt-0.5 text-lg font-[500]">m</span>
-          </div>
-        </div>
-        {selectedType === "wholeRoom" && (
+        {selectedType !== "wholeRoomWithCel" && (
           <div className="flex flex-col gap-y-3">
-            <h1 className="text-xl">Width</h1>
+            <h1 className="text-xl">ความสูง H</h1>
+            <div className="flex gap-x-3 w-full">
+              <NumericInput value={height} setValue={setHeight} />
+              <span className="text-gray-500 mt-0.5 text-lg font-[500]">m</span>
+            </div>
+          </div>
+        )}
+        {(selectedType === "wholeRoom" || selectedType==='wholeRoomWithCel') && (
+          <div className="flex flex-col gap-y-3">
+            <h1 className="text-xl">ความกว้าง W</h1>
             <div className="flex gap-x-3 w-full">
               <NumericInput value={width} setValue={setWidth} />
               <span className="text-gray-500 mt-0.5 text-lg font-[500]">m</span>
             </div>
           </div>
         )}
-        <div className="flex flex-col gap-y-10">
-          <div className="flex gap-x-3 items-center">
-            <h3 className="text-xl ">Number of standard doors</h3>
-            <a className="doors-tooltip">
-              <InfoIcon className="w-5 h-5 mt-[3px]" />
-            </a>
-          </div>
-          <Tooltip anchorSelect=".doors-tooltip" place="right">
-            <p>Include more doors if you've got double doors or open arches</p>
-          </Tooltip>
-          <RangeSlider values={doors} setValues={setDoors} />
-        </div>
-        <div className="flex flex-col gap-y-10">
-          <div className="flex gap-x-3 items-center">
-            <h3 className="text-xl ">Number of standard windows</h3>
-            <a className="windows-tooltip">
-              <InfoIcon className="w-5 h-5 mt-[3px]" />
-            </a>
-          </div>
-          <Tooltip anchorSelect=".windows-tooltip" place="right">
-            <p>
-              Include more windows if you've got bay windows or large patio
-              doors
-            </p>
-          </Tooltip>
-          <RangeSlider values={windows} setValues={setWindows} />
-        </div>
+        {selectedType !== "wholeRoomWithCel" && (
+          <>
+            <div className="flex flex-col gap-y-10">
+              <div className="flex gap-x-3 items-center">
+                <h3 className="text-xl ">จำนวนประตูมาตรฐาน</h3>
+                <a className="doors-tooltip">
+                  <InfoIcon className="w-5 h-5" />
+                </a>
+              </div>
+              <Tooltip anchorSelect=".doors-tooltip" place="right">
+                <p>ประตูมาตรฐานมีขนาดประมาณ 1.6 ตารางเมตร</p>
+              </Tooltip>
+              <RangeSlider values={doors} setValues={setDoors} />
+            </div>
+            <div className="flex flex-col gap-y-10">
+              <div className="flex gap-x-3 items-center">
+                <h3 className="text-xl ">จำนวนหน้าต่างมาตรฐาน</h3>
+                <a className="windows-tooltip">
+                  <InfoIcon className="w-5 h-5" />
+                </a>
+              </div>
+              <Tooltip anchorSelect=".windows-tooltip" place="right">
+                <p>หน้าต่างมาตรฐานมีขนาดประมาณ 0.6 ตารางเมตร</p>
+              </Tooltip>
+              <RangeSlider values={windows} setValues={setWindows} />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
